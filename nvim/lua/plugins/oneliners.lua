359f3f34adb5b8a837266932a306fdc00d443d9b
Noè Murr <Noe.Murr@elt.it>
2024-09-05 16:25:03 +0200

    [FIX] fine-cmdline

--- nvim/lua/plugins/oneliners.lua
+++ nvim/lua/plugins/oneliners.lua
@@ -73,9 +73,29 @@ return {
 
     {
         'VonHeikemen/fine-cmdline.nvim',
+        opts = {
+            cmdline = {
+                prompt = '> ',
+            },
+            popup = {
+                position = {
+                  row = '50%',
+                  col = '50%',
+                },
+                size = {
+                  width = '60%',
+                },
+                border = {
+                  style = 'rounded',
+                },
+                win_options = {
+                  winhighlight = 'Normal:Normal,FloatBorder:FloatBorder',
+                },
+              },
+        },
         keys = {
-	    {":", '<cmd>FineCmdline<CR>', desc = "command line"}
-	},
+	        {":", '<cmd>FineCmdline<CR>', desc = "command line"}
+	    },
         dependencies = { "MunifTanjim/nui.nvim" },
     },
 
